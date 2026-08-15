a9f78cf06d8db464aeaca46cc9da16b2dde885a2
asepaja7408-netizen <asepaja7408@gmail.com>
2026-08-16 03:48:00 +0700

    Update CloneBunker.lua to load Loader.lua

--- PhoneIDViewer/CloneBunker.lua
+++ PhoneIDViewer/CloneBunker.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/asepaja7408-netizen/main/PhoneIDViewer/CloneBunker.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/asepaja7408-netizen/main/Loader.lua"))()
